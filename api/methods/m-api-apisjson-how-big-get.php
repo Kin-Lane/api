20e4066faf2749228b939f2980d1f4153fd3ba2d
Kin Lane <info@apievangelist.com>
2015-11-27 16:21:35 -0800

    add new endpoint

---
+++ api/methods/m-api-apisjson-how-big-get.php
@@ -0,0 +1,104 @@
+<?php
+
+$route = '/api/apisjson/how-big/';
+$app->get($route, function ()  use ($app,$contentType,$githuborg,$githubrepo){
+
+	$ReturnObject = array();
+
+	$ReturnObject = array();
+	$ReturnObject['contentType'] = $contentType;
+
+	if($contentType == 'application/apis+json')
+		{
+		$app->response()->header("Content-Type", "application/json");
+
+		$apis_json_url = "http://" . $githuborg . ".github.io/" . $githubrepo . "/apis.json";
+		$apis_json = file_get_contents($apis_json_url);
+		echo stripslashes(format_json($apis_json));
+		}
+	else
+		{
+		if(isset($_REQUEST['apisjson_url'])){ $apisjson_url = $_REQUEST['apisjson_url']; } else { $apisjson_url = '';}
+
+		$apis_json_results = file_get_contents($apisjson_url);
+
+		$apis_json_results = file_get_contents($apis_json_results);
+
+		$apis_json = json_decode($apis_json_results);
+		$oadf_apis = $apis_json->apis;
+
+		$NumberofPaths = 0;
+		$NumberofVerbs = 0;
+		$NumberofParameters = 0;
+		$NumberofResponses = 0;
+		$numberofDefinitions = 0;
+
+		foreach($oadf_apis as $apis)
+			{
+			// Begin each API
+			foreach($apis->properties as $apis_properties)
+				{
+				// Begin Each Property
+				$type = $apis_properties->type;
+
+				if($type=="Swagger")
+					{
+
+					$oadf_url = $apis_properties->url;
+
+					//echo $oadf_url . "<br />";
+
+					$oadf_json = file_get_contents($oadf_url);
+					$apis_path = json_decode($oadf_json,true);
+
+					$ThisPaths = 0;
+					$ThisVerbs = 0;
+					$ThisParameters = 0;
+					$ThisResponses = 0;
+					$ThisDefinitions = 0;
+
+					foreach($apis_path['paths'] as $key => $value)
+						{
+						$ThisPaths++;
+						foreach($value as $key2 => $value2)
+							{
+							$ThisVerbs++;
+							foreach($value2['parameters'] as $parameters)
+								{
+								$ThisParameters++;
+								}
+							foreach($value2['responses'] as $responses)
+								{
+								$ThisResponses++;
+								}
+							}
+						}
+
+					foreach($apis_path['definitions'] as $definitions)
+						{
+						$ThisDefinitions++;
+						}
+
+					$NumberofPaths = $NumberofPaths + $ThisPaths;
+					$NumberofVerbs = $NumberofVerbs + $ThisVerbs;
+					$NumberofParameters = $NumberofParameters + $ThisParameters;
+					$NumberofResponses = $NumberofResponses + $ThisResponses;
+					$numberofDefinitions = $numberofDefinitions + $ThisDefinitions;
+
+					}
+				}
+			}
+
+		$ReturnObject['paths'] = $NumberofPaths;
+		$ReturnObject['verbs'] = $NumberofVerbs;
+		$ReturnObject['parameters'] = $NumberofParameters;
+		$ReturnObject['responses'] = $NumberofResponses;
+		$ReturnObject['definitions'] = $numberofDefinitions;
+
+		$app->response()->header("Content-Type", "application/json");
+		echo stripslashes(format_json(json_encode($ReturnObject)));
+
+		}
+	});
+
+?>
