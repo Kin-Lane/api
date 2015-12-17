97ab9f487a686d6e834c311e73ff0ab3ba88e14b
Kin Lane <info@apievangelist.com>
2015-12-16 23:10:25 -0800

    debug

--- api/methods/m-api-apisjson-explode-oadf-get.php
+++ api/methods/m-api-apisjson-explode-oadf-get.php
@@ -176,13 +176,14 @@ $app->get($route, function ()  use ($app,$contentType,$githuborg,$githubrepo){
 												}
 											}
 										}
+
+										$Explode[$Break]['paths'] = new stdClass;
+										$Explode[$Break]['paths'] = $ThisPaths;
+										$Explode[$Break]['definitions'] = new stdClass;
+										$Explode[$Break]['definitions'] = $ThisDefinitions;
+
+
 									}
-
-								$Explode[$Break]['paths'] = new stdClass;
-								$Explode[$Break]['paths'] = $ThisPaths;
-								$Explode[$Break]['definitions'] = new stdClass;
-								$Explode[$Break]['definitions'] = $ThisDefinitions;
-
 								}
 							}
 
